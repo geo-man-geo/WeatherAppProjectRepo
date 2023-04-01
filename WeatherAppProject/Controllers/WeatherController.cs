5e3ce1902e19dac738974c7a0a499bf2129a440d
Leo Mathews George <leomathewsgeorge@gmail.com>
2023-04-01 20:35:36 +0530

    first commit

---
+++ WeatherAppProject/Controllers/WeatherController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherAppProject.Models;
+
+namespace WeatherAppProject.Controllers
+{
+    public class WeatherController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            List<CityWeather> cityWeather = new List<CityWeather>() { 
+                new CityWeather() {CityUniqueCode = "LDN", CityName = "London", DateAndTime = Convert.ToDateTime("2030-01-01 8:00"),  TemperatureFahrenheit = 33},
+                new CityWeather() {CityUniqueCode = "NYC", CityName = "New York", DateAndTime = Convert.ToDateTime("2030-01-01 3:00"),  TemperatureFahrenheit = 60},
+                new CityWeather() {CityUniqueCode = "PAR", CityName = "Paris", DateAndTime = Convert.ToDateTime("2030-01-01 9:00"),  TemperatureFahrenheit = 82}
+            };
+            ViewBag.cityWeather = cityWeather;
+            return View();
+        }
+
+        [Route("weather/{cityCode}")]
+        public IActionResult Weather(string cityCode)
+        {
+            List<CityWeather> cityWeather = new List<CityWeather>() {
+                new CityWeather() {CityUniqueCode = "LDN", CityName = "London", DateAndTime = Convert.ToDateTime("2030-01-01 8:00"),  TemperatureFahrenheit = 33},
+                new CityWeather() {CityUniqueCode = "NYC", CityName = "New York", DateAndTime = Convert.ToDateTime("2030-01-01 3:00"),  TemperatureFahrenheit = 60},
+                new CityWeather() {CityUniqueCode = "PAR", CityName = "Paris", DateAndTime = Convert.ToDateTime("2030-01-01 9:00"),  TemperatureFahrenheit = 82}
+            };
+
+            var cityList = cityWeather.Where(city => city.CityUniqueCode == cityCode).ToList();
+            ViewBag.cityList = cityList;
+            return View(cityList);
+        }
+    }
+}
---
+++ WeatherAppProject/Models/CityWeather.cs
@@ -0,0 +1,12 @@
+﻿namespace WeatherAppProject.Models
+{
+    public class CityWeather
+    {
+        public string? CityName { get; set; }
+        public string? CityUniqueCode { get; set; }
+        public DateTime? DateAndTime { get; set; }  
+        public int? TemperatureFahrenheit { get; set; }  
+
+
+    }
+}
